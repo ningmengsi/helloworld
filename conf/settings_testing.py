99d75b8f321feaacdd42066c832b77d22b286c88
ningmengsi <wujunlmp@163.com>
2019-03-08 16:48:26 +0800

    j

--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -21,8 +21,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
         'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                            # 你的数据库user
-        'PASSWORD': 'lenovo',                        # 你的数据库password
-        'HOST': '192.168.30.135',              		   # 数据库HOST
+        'PASSWORD': 'YtX#_]O[6n',                        # 你的数据库password
+        'HOST': '10.103.238.207',              		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
